eec2559fda4b4fac851d958767550c0f0b6c9a05
Weiyan Zhu <weiyanz@uci.edu>
2019-01-31 15:51:25 -0800

    player shoot bullet

--- Ghost Invader/Assets/Scripts/Player/PlayerBullet.cs
+++ Ghost Invader/Assets/Scripts/Player/PlayerBullet.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class PlayerBullet : MonoBehaviour
 {
     public Vector2 velocity;
+    public float halfHeight; 
+    private Camera ourCamera;
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<Rigidbody2D>().velocity = velocity;
+        ourCamera = GameObject.FindObjectOfType<Camera>();
+    }
+
+    void Update()
+    {
+        CheckBound();
     }
 
     public void OnTriggerEnter2D(Collider2D other)
@@ -20,9 +28,17 @@ public class PlayerBullet : MonoBehaviour
         }
     }
 
+    public void CheckBound()
+    {
+        if(ourCamera.WorldToScreenPoint(transform.position - new Vector3(0, halfHeight, 0)).y > Screen.height)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     public void OnDestroy()
     {
-        
+        GameObject.FindObjectOfType<Player>().ResetBullet();
     }
 
 }
