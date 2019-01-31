efc97b5b26c90945010550614643f428d2f6f6f2
Weiyan Zhu <weiyanz@uci.edu>
2019-01-31 13:47:32 -0800

    player movement

--- Ghost Invader/Assets/Scripts/Player/Player.cs
+++ Ghost Invader/Assets/Scripts/Player/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
+    public float halfWidth;
+    public Camera ourCamera;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,23 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    public void Move()
+    {
+        //move ment
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed * 60 * Time.deltaTime ,0);
+        //boundary
+        //check left
+        if(ourCamera.WorldToScreenPoint(transform.position - new Vector3(halfWidth, 0, 0)).x < 0)
+        {
+            transform.position = new Vector3(ourCamera.ScreenToWorldPoint(new Vector3(0,0,0)).x + halfWidth, transform.position.y, transform.position.z);
+        }
+        //check right
+        else if(ourCamera.WorldToScreenPoint(transform.position + new Vector3(halfWidth, 0, 0)).x > Screen.width)
+        {
+            transform.position = new Vector3(ourCamera.ScreenToWorldPoint(new Vector3(Screen.width,0,0)).x - halfWidth, transform.position.y, transform.position.z);
+        }
     }
 }
