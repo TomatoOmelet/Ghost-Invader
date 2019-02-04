f4161a7c97a207575dfd9148513d9e4efc91b548
Weiyan Zhu <weiyanz@uci.edu>
2019-02-03 18:56:21 -0800

    score UI

---
+++ Ghost Invader/Assets/Scripts/Manager/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private int score = 0;
+    public UIManager uiManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        uiManager = GameObject.FindObjectOfType<UIManager>();
+        uiManager.UpdateScore(score);
+        AddScore(500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddScore(int value)
+    {
+        StartCoroutine(AddScoreRoutine(value));
+    }
+
+    public IEnumerator AddScoreRoutine(int value)
+    {
+        for(int x = 0; x < value; ++x)
+        {
+            ++score;
+            uiManager.UpdateScore(score);
+            yield return null;
+        }
+    }
+
+}
