1ead5d88f7dd54f3c1032fa760353193db629777
Weiyan Zhu <weiyanz@uci.edu>
2019-02-03 19:38:47 -0800

    Shield base

---
+++ Ghost Invader/Assets/Scripts/Items/Shield.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+
+    private int health = 4;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "PlayerBullet" || other.tag == "EnemyBullet")
+        {
+            //destroy bullet
+            Destroy(other.gameObject);
+            --health;
+            //change color
+            Color newColor = spriteRenderer.color;
+            newColor.a -= 0.3f;
+            spriteRenderer.color = newColor;
+            //broken
+            if(health<= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
