1046f87b3369fa005526779fa18cc330fc6e949a
Weiyan Zhu <weiyanz@uci.edu>
2019-02-03 19:59:03 -0800

    Update LevelManager.cs

--- Ghost Invader/Assets/Scripts/Manager/LevelManager.cs
+++ Ghost Invader/Assets/Scripts/Manager/LevelManager.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
+    public static bool hasManager = false;
     private int score = 0;
     public UIManager uiManager;
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
+        if(hasManager == false)
+        {
+            DontDestroyOnLoad(gameObject);
+            hasManager = true;
+        }
+        else
+            Destroy(gameObject);
         uiManager = GameObject.FindObjectOfType<UIManager>();
         uiManager.UpdateScore(score);
     }
@@ -33,6 +41,13 @@ public class LevelManager : MonoBehaviour
             uiManager.UpdateScore(score);
             yield return null;
         }
+    }
+
+    public void GoToNewLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        uiManager = GameObject.FindObjectOfType<UIManager>();
+        uiManager.UpdateScore(score);
     }
 
 }
