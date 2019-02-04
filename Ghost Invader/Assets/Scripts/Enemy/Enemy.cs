fa949a81ae72d060d3af6e73481e1149bdcd3dff
SupriseOrb <molinakmey@gmail.com>
2019-02-03 19:18:27 -0800

    Enemy Movement

--- Ghost Invader/Assets/Scripts/Enemy/Enemy.cs
+++ Ghost Invader/Assets/Scripts/Enemy/Enemy.cs
@@ -4,50 +4,24 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public int enemySpeed = 1;
-    public int down = -1;
-    public bool canMoveRight = true;
-    public Rigidbody2D rb;
+    public EnemyManager enemyManager;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        enemyManager = this.transform.parent.GetComponent<EnemyManager>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        move();
-    }
-
-    void move()
-    {
-        if(canMoveRight)
-        {
-            Debug.Log("canMoveRight");
-            rb.velocity = new Vector2(enemySpeed, 0);
-        }
-        else
-        {
-            Debug.Log("canMoveLeft");
-            rb.velocity = new Vector2(-enemySpeed, 0);
-        }
+        
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("OnCollisionEnter2D");
-        if (collision.gameObject.tag == "RightBound")
+        if (collision.gameObject.tag == "RightBound" || collision.gameObject.tag == "LeftBound")
         {
-            transform.position += new Vector3(0, down * Time.deltaTime, 0);
-            canMoveRight = false;
-        }
-        else if (collision.gameObject.tag == "LeftBound")
-        {
-            //rb.velocity = new Vector2(0, down * 60 * Time.deltaTime);
-            transform.position += new Vector3(0, down * Time.deltaTime, 0);
-            canMoveRight = true;
+            enemyManager.switchDirection();
         }
     }
 }
---
+++ Ghost Invader/Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    
+    public bool canMoveRight = true;
+    public int enemySpeed = 1;
+    public int down = -1;
+
+    public Enemy enemyPrefab;
+
+    [Header("Enemy Array Info")]
+    public int ColumnLength;
+    public int RowHeight;
+    public float colStart;
+    public float rowStart;
+    public float colSpace;
+    public float rowSpace;
+    public Enemy[,] enemyArray;
+
+
+
+    void Awake()
+    {
+        enemyArray = new Enemy[ColumnLength, RowHeight];
+        
+        for (int i = 0; i < ColumnLength; ++i)
+        {
+            for (int j = 0; j < RowHeight; ++j)
+            {
+                enemyArray[i, j] = Instantiate(enemyPrefab, new Vector3(colStart + colSpace * i, rowStart + rowSpace * j, 0), Quaternion.identity);
+                enemyArray[i, j].transform.SetParent(transform);
+            }
+        }
+    }
+
+    void Update()
+    {
+        move();
+    }
+
+    void move()
+    {
+        if (canMoveRight)
+        {
+            transform.position += new Vector3(enemySpeed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            transform.position += new Vector3(-enemySpeed * Time.deltaTime, 0, 0);
+        }
+    }
+
+    public void switchDirection()
+    {
+        transform.position += new Vector3(0, down * Time.deltaTime, 0);
+        canMoveRight = !canMoveRight;
+    }
+}
