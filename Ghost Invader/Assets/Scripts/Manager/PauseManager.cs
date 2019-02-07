dbe886f3582bd5f6424ce3176585a2aff1624904
SupriseOrb <molinakmey@gmail.com>
2019-02-07 15:38:32 -0800

    Made Fully Functional Pause Menu

---
+++ Ghost Invader/Assets/Scripts/Manager/PauseManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    bool isPaused = false;
+    public GameObject pausePanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused == false)
+            {
+                pause();
+            }
+            else if(isPaused == true)
+            {
+                resume();
+            }
+        }
+    }
+
+    public void pause()
+    {
+        Time.timeScale = 0.0f;
+        isPaused = true;
+        pausePanel.SetActive(true);
+    }
+
+    public void resume()
+    {
+        Time.timeScale = 1.0f;
+        isPaused = false;
+        pausePanel.SetActive(false);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+
+}
