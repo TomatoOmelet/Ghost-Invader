3e53ff54225bd30f30d14c1da2a02d28b879cf78
Weiyan Zhu <weiyanz@uci.edu>
2019-02-07 16:54:01 -0800

    UFO

---
+++ Ghost Invader/Assets/Scripts/Manager/UFOManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFOManager : MonoBehaviour
+{
+    public GameObject UFO;
+    public float initialWait;
+    public float minWaitTime;
+    public float maxWaitTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GenerateUFO());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator GenerateUFO()
+    {
+        yield return new WaitForSeconds(initialWait);
+        while(true)
+        {
+            GameObject.Instantiate(UFO);
+            yield return new WaitForSeconds(Random.Range(minWaitTime, maxWaitTime));
+        }
+    }
+
+}
